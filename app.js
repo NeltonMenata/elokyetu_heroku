712e486a22b73b0a5aa45883da303799a624d251
Nelton Menata <77100633+NeltonMenata@users.noreply.github.com>
2022-03-15 20:31:55 +0100

    Update app.js
    Adicionando o express

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-function main(){
-    console.log("Hello Heroku");
-}
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res)=>{
+    res.send("Hola dev Heroku + NodeJS!!");
+});
+
+app.listen(port, ()=>{
+    console.log("Rodando o app no Heroku");
+});
